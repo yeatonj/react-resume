1f9324246297610c1e43b0ccad185a58db96dac2
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-15 10:12:42 -0400

    Remove unused logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import PersonalInfo from './components/PersonalInfo'
 import Dropdown from './components/Dropdown'
 import './App.css'
