7a7c318235b9a7be962d94e1a740cea4a4302905
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-15 10:02:30 -0400

    Add resume layout components

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,48 @@ function App() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Doe");
   const [email, setEmail] = useState("test_email@notasite.com");
+  const [phone, setPhone] = useState("(123) 123-1234");
+  const [summary, setSummary] = useState("This is an example summary. Use this section to talk a little bit about yourself and give an overview of your skills and areas of expertise");
+  const [edInfo, setEdInfo] = useState([
+    {
+      name:"Test Univ",
+      city:"Ex City",
+      state:"Ex State",
+      start:"Jan 2020",
+      end:"Aug 2024",
+      degree:"BS Example Subject",
+      notes:["Dean's list", "Did a pretty cool project"]
+    },
+    {
+      name:"Other Univ",
+      city:"Other city",
+      state:"Other state",
+      start:"Jan 2025",
+      end:"Aug 2025",
+      degree:"MS Example Subject",
+      notes:["4.0 GPA", "Was Awesome"]
+    }
+  ]);
+  const [workInfo, setWorkInfo] = useState([
+    {
+      company:"Test Company",
+      city:"Ex City",
+      state:"Ex State",
+      start:"Jan 2020",
+      end:"Aug 2024",
+      title:"CEO",
+      notes:["Worked on some fun stuff", "Did a cool demo", "Won employee of the month"]
+    },
+    {
+      company:"Other company",
+      city:"Other city",
+      state:"Other state",
+      start:"Jan 2023",
+      end:"Sept 2025",
+      title:"",
+      notes:["Nice project"]
+    }
+  ])
 
   return (
     <>
@@ -27,6 +69,10 @@ function App() {
         firstName={firstName}
         lastName={lastName}
         email={email}
+        phone={phone}
+        summary={summary}
+        educationInfo={edInfo}
+        workInfo={workInfo}
       />
     </>
   )
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,127 @@
 export default function Resume({
     firstName,
     lastName,
-    email
+    email,
+    phone,
+    summary,
+    educationInfo,
+    workInfo
 }) {
     return (
-        <p>{firstName + " " + lastName + " " + email}</p>
+        <section className="resume">
+            <HeaderSection 
+                firstName={firstName}
+                lastName={lastName}
+                email={email}
+                phone={phone}
+            />
+            <SummarySection 
+                summary={summary}
+            />
+            <EducationSection 
+                educationInfo={educationInfo}
+            />
+            <WorkSection 
+                workInfo={workInfo}
+            />
+        </section>
+    )
+}
+
+function HeaderSection({
+    firstName,
+    lastName,
+    email,
+    phone
+}) {
+    return (
+        <div className="resume-header">
+            <h1>{firstName + " " + lastName}</h1>
+            <div className="contact">
+                <h3>{email}</h3>
+                <h3>{phone}</h3>
+            </div>
+        </div>
+    )
+}
+
+function SummarySection({ summary }) {
+    return (
+        <div className="resume-summary">
+            <h2>Summary</h2>
+            <p>{summary}</p>
+        </div>
+    )
+}
+
+function EducationSection({ educationInfo }) {
+    if (educationInfo.length === 0) {
+        return (
+            <div className="resume-education">
+                <h2>Education</h2>
+                <p>No education history to display</p>
+            </div>
+        )
+    }
+    // Else, we'll return the list of education
+    return (
+        <div className="resume-education">
+            <h2>Education</h2>
+            <div className="education-cards">
+                {educationInfo.map(info =>
+                    <div className="ed-card" key={info.start}>
+                        <div className="ed-header">
+                            <h3>{info.name}</h3>
+                            <p>{info.city}</p>
+                            <p>{info.start + " - " + info.end}</p>
+                        </div>
+                        <h3>{info.degree}</h3>
+                        <ul>
+                            {info.notes.map(note =>
+                                <li key={note}>
+                                    {note}
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+function WorkSection({ workInfo }) {
+    if (workInfo.length === 0) {
+        return (
+            <div className="resume-work">
+                <h2>Work History</h2>
+                <p>No work history to display</p>
+            </div>
+        )
+    }
+    // Else, we'll return the list of education
+    return (
+        <div className="resume-work">
+            <h2>Work History</h2>
+            <div className="work-cards">
+                {workInfo.map(info =>
+                    <div className="work-card" key={info.start}>
+                        <div className="work-header">
+                            <h3>{info.company}</h3>
+                            <p>{info.city}</p>
+                            <p>{info.start + " - " + info.end}</p>
+                        </div>
+                        <h3>{info.degree}</h3>
+                        <ul>
+                            {info.notes.map(note =>
+                                <li key={note}>
+                                    {note}
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
